d1bd1500a539fbc989fd6414fab94e2f87590c4e
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2021-09-28 14:57:09 -0300

    array de condições climáticas para tratamento dos dados

---
+++ condicoes_climaticas.js
@@ -0,0 +1,60 @@
+let condicoes = [
+    "Sol", //DIA
+    "Céu limpo", //NOITE
+    //NUBLADO
+    "Parcialmente nublado",
+    "Nublado",
+    "Encoberto",
+    "Neblina",
+    "Nevoeiro",
+    "Nevoeiro gelado",
+    //CHUVA
+    "Possibilidade de chuva irregular",
+    "Possibilidade de chuvisco gelado irregular",
+    "Aguaceiros fracos com granizo",
+    "Aguaceiros moderados ou fortes com granizo",
+    "Chuva fraca irregular com trovoada",
+    "Chuva moderada ou forte com trovoada",
+    "Possibilidade de trovoada",
+    "Chuvisco irregular",
+    "Chuvisco",
+    "Chuvisco gelado",
+    "Chuvisco forte gelado",
+    "Chuva fraca irregular",
+    "Chuva fraca",
+    "Períodos de chuva moderada",
+    "Chuva moderada",
+    "Períodos de chuva forte",
+    "Chuva forte",
+    "Chuva fraca e gelada",
+    "Chuva gelada moderada ou forte",
+    "Chuva fraca com neve",
+    "Chuva forte ou moderada com neve",
+    "Granizo",
+    "Aguaceiros fracos",
+    "Aguaceiros moderados ou fortes",
+    "Chuva torrencial",
+    "Aguaceiros fracos com neve",
+    "Aguaceiros moderados ou fortes com neve",
+    "Chuva fraca com neve",
+    "Chuva moderada ou forte com neve",
+  
+    //NEVE
+    "Possibilidade de neve irregular",
+    "Possibilidade de neve molhada irregular",
+    "Rajadas de vento com neve",
+    "Nevasca",
+    "Queda de neve irregular e fraca",
+    "Queda de neve fraca",
+    "Queda de neve moderada e irregular",
+    "Queda de neve moderada",
+    "Queda de neve forte e irregular",
+    "Neve intensa",
+    "Neve fraca irregular com trovoada",
+    "Neve moderada ou forte com trovoada",
+  ];
+
+  
+let condicoesNublado = condicoes.slice(2,8)
+let condicoesChuva = condicoes.slice(8,37)
+let condicoesNeve = condicoes.slice(37)
